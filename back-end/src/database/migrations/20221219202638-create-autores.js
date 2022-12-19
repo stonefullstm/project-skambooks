bd438aad214511c35938b070f13c8f0100417b8a
stonefullstm <stonefull.stm@gmail.com>
2022-12-19 17:33:59 -0300

    Incluindo migation de autores

---
+++ back-end/src/database/migrations/20221219202638-create-autores.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('autores', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      nome: {
+        allowNull: false,
+        type: Sequelize.STRING(255),
+      },
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('autores');
+  },
+};
