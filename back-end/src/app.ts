a8ee954cb45e52225a604caa55333a0325508e49
Reinaldo <reinaldoper83@gmail.com>
2022-12-21 16:07:44 -0300

    perdido

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,11 +1,9 @@
 import express from 'express';
 import routerLivros from './routes/livros.routes';
-import routerLeitor from './routes/leitores.routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(routerLivros);
-app.use(routerLeitor);
 
 export default app;
--- back-end/src/routes/leitores.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import leitores from '../controllers/leitores.controller';
-
-const routerLeitor = Router();
-routerLeitor.get('/leitores/:id', leitores.getIdLeitor);
-
-export default routerLeitor;
