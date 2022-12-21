aba72f6dc6fbd0bb593004393ecd4a0cf9b15c59
Reinaldo <reinaldoper83@gmail.com>
2022-12-21 16:11:10 -0300

    adicionado getIdLeitor

---
+++ back-end/src/types/Tleitores.ts
@@ -0,0 +1,15 @@
+export type Tleitores = {
+  id?: number;
+  nome: string;
+  endereco: string;
+  numero: string;
+  complemento: string;
+  cep: string;
+  bairro: string;
+  cidade: string;
+  estado: string;
+  telefone: string;
+  email: string;
+  senha: string;
+  creditos: number;
+};
