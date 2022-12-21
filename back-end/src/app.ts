0b65d2300d9c64aa94933fbbf9fed66c4102e2ee
Reinaldo <reinaldoper83@gmail.com>
2022-12-21 15:54:47 -0300

    getIdLeitor router

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import routerLivros from './routes/livros.routes';
+import routerLeitor from './routes/leitores.routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(routerLivros);
+app.use(routerLeitor);
 
 export default app;
---
+++ back-end/src/controllers/leitores.controller.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+import leitores from '../services/leitores.service';
+import statusCodes from '../statusCodes';
+
+const getIdLeitor = async ( req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await leitores.getIdLeitor(Number(id));
+  if (result) res.status(statusCodes.OK).json({result});
+  res.status(statusCodes.NOT_FOUND).json({ message: 'Leitor not found'});
+};
+
+export default { getIdLeitor };
