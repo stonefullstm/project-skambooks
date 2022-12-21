815be4387623ee46b4e8b82f6dc5bcca71e69b0d
Reinaldo <reinaldoper83@gmail.com>
2022-12-21 18:23:17 -0300

    post leitores

--- back-end/src/services/leitores.service.ts
+++ back-end/src/services/leitores.service.ts
@@ -8,4 +8,10 @@ const getIdLeitor = async (id: number): Promise<Tleitores[]> => {
   return leitor as unknown as Tleitores[];
 };
 
-export default { getIdLeitor };
+const insertLeitor = async (body: Tleitores): Promise<Tleitores[]> => {
+  const { nome, endereco, numero, complemento, cep, bairro, cidade, estado, telefone, email, senha, creditos } = body;
+  const result = await leitores.create({nome, endereco, numero, complemento, cep, bairro, cidade, estado, telefone, email, senha, creditos});
+  return result as unknown as Tleitores[];
+};
+
+export default { getIdLeitor, insertLeitor };
