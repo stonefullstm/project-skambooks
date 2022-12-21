e26542d5f438907caf39fa1d268e4c0951d523ce
Reinaldo <reinaldoper83@gmail.com>
2022-12-21 17:03:27 -0300

    ajuste retorno do getIdLeitor

--- back-end/src/controllers/leitores.controller.ts
+++ back-end/src/controllers/leitores.controller.ts
@@ -5,8 +5,10 @@ import statusCodes from '../statusCodes';
 const getIdLeitor = async ( req: Request, res: Response) => {
   const { id } = req.params;
   const result = await leitores.getIdLeitor(Number(id));
-  if (result) res.status(statusCodes.OK).json({result});
-  res.status(statusCodes.NOT_FOUND).json({ message: 'Leitor not found'});
+  if (result) {
+    return res.status(statusCodes.OK).json(result);
+  }
+  return res.status(statusCodes.NOT_FOUND).json({ message: 'Leitor not found'});
 };
 
 export default { getIdLeitor };
