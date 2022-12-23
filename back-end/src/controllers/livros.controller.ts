79beeb136b449e43afa46125a51f65309ff968b2
Reinaldo <reinaldoper83@gmail.com>
2022-12-23 13:10:46 -0300

    inserir livro

--- back-end/src/controllers/livros.controller.ts
+++ back-end/src/controllers/livros.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import livrosService from '../services/livros.service';
 import statusCodes from '../statusCodes';
+import { TAutor } from '../types';
 
 const getAll = async (req: Request, res: Response) => {
   const livros = await livrosService.getAll();
@@ -21,7 +22,18 @@ const deleteLivro = async ( req: Request, res: Response) => {
   
 };
 
+const createLivro = async (req: Request, res: Response) => {
+  const { isbn, titulo, ano, paginas, idLeitor, autores } = req.body;
+  await livrosService.createLivro({isbn, titulo, ano, paginas, idLeitor, autores});
+  const idLivro = (await livrosService.getAll()).length;
+  const promisse = await autores.map((i: TAutor) => livrosService.createAutor(i));
+  const a = await promisse.map((item: { id: number; }) => livrosService.createAutorLivro(idLivro, item.id));
+  await Promise.all(a);
+  return res.status(statusCodes.CREATED).json(req.body);
+};
+  
 export default {
   getAll,
   deleteLivro ,
+  createLivro,
 };
--- back-end/src/routes/livros.routes.ts
+++ back-end/src/routes/livros.routes.ts
@@ -4,5 +4,6 @@ import livrosController from '../controllers/livros.controller';
 const routerLivros = Router();
 routerLivros.delete('/livros/:id', livrosController.deleteLivro);
 routerLivros.get('/livros', livrosController.getAll);
+routerLivros.post('/livros', livrosController.createLivro);
 
 export default routerLivros;
