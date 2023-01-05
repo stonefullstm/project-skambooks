461cbd4732724af27c8081195c6a44c6f4f6584c
Reinaldo <reinaldoper83@gmail.com>
2023-01-05 20:31:30 -0300

    adicionado select users

--- front-end/src/actions/action.jsx
+++ front-end/src/actions/action.jsx
@@ -1,5 +1,7 @@
 export const NAMEINPUT = 'NAMEINPUT';
+export const READERS = 'READERS';
 
 export const requiretBooks = (payload) => ({ type: NAMEINPUT, payload });
+export const requiretReaders = (payload) => ({ type: READERS, payload });
 
 
--- front-end/src/reducers/reducer.jsx
+++ front-end/src/reducers/reducer.jsx
@@ -1,14 +1,17 @@
 import {
   NAMEINPUT,
+  READERS,
 } from '../actions/action';
 
 const INICIAL_STATE = {
   books: [],
+  reader: [],
 };
 
 const reducerFetch = (state = INICIAL_STATE, action) => {
   switch (action.type) {
   case NAMEINPUT: return ({ ...state, books: action.payload });
+  case READERS: return ({ ...state, reader: action.payload });
   default: return state;
   }
 };
