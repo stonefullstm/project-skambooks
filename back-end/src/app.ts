9031a6357178af47e3f7100cfe6a2ec62db04aa7
stonefullstm <stonefull.stm@gmail.com>
2023-01-10 18:31:52 -0300

    fix: Ajustando os fetch para adequar ao railway

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,4 +1,3 @@
-import cors from 'cors';
 import express from 'express';
 import morgan from 'morgan';
 import routerBooks from './routes/books.routes';
@@ -6,7 +5,7 @@ import routerExchanges from './routes/exchanges.routes';
 import routerReader from './routes/readers.routes';
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 
 app.use(express.json());
 app.use(morgan('tiny'));
